4da25895635b19700a6ee71f068edf20533ba212
Deepak Suyal <deepaksuyal310@gmail.com>
2025-04-20 08:25:33 +0530

    deploy commit

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -3,9 +3,9 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import Header from "@/app/components/Header";
-import SideNavbar from "@/app/components/SideNavbar";
-import Footer from "@/app/components/Footer";
+import Header from "../components/Header";
+import SideNavbar from "../components/SideNavbar";
+import Footer from "../components/Footer";
 
 // Blog posts data with markdown content
 const blogPosts = {
